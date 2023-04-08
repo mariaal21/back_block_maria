0c532a5b7ca83715332d97c3699c03c58553a879
mariaal21 <malmazandelpie@gmail.com>
2023-04-08 13:31:37 +0200

    Primeros

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+
+
+
+port = process.env.PORT || 4010;
+
+app.use(cors());                                    
+app.use(express.static(__dirname + '/public'));  
+
+app.use(express.urlencoded({ extended: false }))    
+app.use(express.json())                            
+
+
+// app.use('/api/users', require('./routers/routerUsers'));        //User routers
+// app.use('/api/entries', require('./routers/routerEntries'));    //Entries routers
+
+
+
+//404
+app.use((req, res, next) => {
+
+    res.status(404).render('404', {
+        error: '404',
+        msg: 'Página no encontrada'
+    });
+
+});
+
+
+
+app.listen(port, () => console.log(`Server listenning on port ${port}...`));
